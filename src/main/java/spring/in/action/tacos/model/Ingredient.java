85c152ba14cbe516cfd05796d85f843ca661d42b
gos1984 <gos1984@list.ru>
2024-10-24 11:15:05 +0300

    part two

---
+++ src/main/java/spring/in/action/tacos/model/Ingredient.java
@@ -0,0 +1,11 @@
+package spring.in.action.tacos.model;
+
+import lombok.Data;
+
+@Data
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+}
---
+++ src/main/java/spring/in/action/tacos/model/Type.java
@@ -0,0 +1,5 @@
+package spring.in.action.tacos.model;
+
+public enum Type {
+    WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+}
